4516be7e7b1eadbade584d6ce6ace915ff3f0856
José Eduardo <141164334+EduardoLCavalcante@users.noreply.github.com>
2024-10-03 16:12:59 -0300

    Update PrivateRoute.tsx

--- src/components/PrivateRoute/PrivateRoute.tsx
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -1,31 +1,44 @@
+import { ReactNode, useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
-import { useEffect, useState } from 'react';
 import { supabase } from '@/lib/supabase'; // Importe sua instância do Supabase
-import HomePage from '@/pages/Home/home';
 import Loading from 'components/Loading/Loading';
 
-const PrivateRoute: React.FC<{ children: React.ReactNode }> = () => {
+type PrivateRouteProps = {
+  children: ReactNode;
+};
+
+const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
   const [loading, setLoading] = useState(true);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   useEffect(() => {
-    const checkUser = async () => {
-      const { data } = await supabase.auth.getUser();
-      setIsAuthenticated(!!data.user); // Define como true se o usuário estiver logado
+    // Verifica se o usuário está autenticado ao inicializar o componente
+    const checkAuthentication = () => {
+      const user = supabase.auth.getUser(); // Obtém o usuário atual
+      setIsAuthenticated(!!user); // Define como true se houver um usuário logado
       setLoading(false); // Finaliza o carregamento
     };
 
-    checkUser();
+    checkAuthentication();
+
+    // Adiciona um listener para atualizar o status de autenticação quando a sessão mudar
+    const { data: authListener } = supabase.auth.onAuthStateChange((_event, session) => {
+      setIsAuthenticated(!!session?.user); // Atualiza a autenticação baseada na existência do usuário na sessão
+    });
+
+    return () => {
+      authListener.subscription.unsubscribe(); // Desinscreve-se ao desmontar o componente
+    };
   }, []);
 
   if (loading) {
-    return <Loading/>; // Opcional: Exibe um carregando enquanto verifica a autenticação
+    return <Loading />; // Exibe algo enquanto carrega/verifica autenticação
   }
 
   return isAuthenticated ? (
-    <HomePage /> // Se estiver logado, renderiza a página inicial
+    <>{children}</> // Renderiza os filhos se o usuário estiver autenticado
   ) : (
-    <Navigate to="/" /> // Se não estiver, redireciona para a página de login
+    <Navigate to="/" replace /> // Redireciona para a página de login se não estiver autenticado
   );
 };
 
