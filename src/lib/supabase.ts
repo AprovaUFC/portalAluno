e5a14b4c58ea7ddd59f81032c041507c4d539a3c
EduardoLCavalcante <eduardocavalcante131@gmail.com>
2025-03-17 15:32:00 -0300

    Arquivo supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,3 +1,6 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient("https://cykmpzlfssdpbchxrult.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN5a21wemxmc3NkcGJjaHhydWx0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUzMDU2NDUsImV4cCI6MjA1MDg4MTY0NX0.OspTdq5RA78Vc5I5WZ3QlFagD8oR5oWBSA8xnOCUo6o");
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY!;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
